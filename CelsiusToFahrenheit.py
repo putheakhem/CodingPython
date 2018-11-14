fc5092fe2ba2038ec105b2d6d3d61a1e95282f9b
Khem Puthea <putheakhem@users.noreply.github.com>
2018-11-14 15:43:00 +0700

    Create CelsiusToFahrenheit.py

---
+++ CelsiusToFahrenheit.py
@@ -0,0 +1,11 @@
+'''
+    Write a program that reads a Celsius degree from the console 
+      and converts it to Fahrenheit and displays the result
+      
+      Written by Mr KHEM PUTHEA
+
+'''
+
+celsius = eval(input("Enter a degree in celsius :"))
+fahrenheit = celsius * (9 / 5) + 32
+print(celsius, "celsius is", fahrenheit, "fahrenheit")
